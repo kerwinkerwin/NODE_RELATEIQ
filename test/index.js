19eb207f0692a5041ec89e6742073de8ae752aa5
kerwinkerwin <kerwin@enspiral.com>
2015-08-12 11:33:21 +1200

    tests for get /contacts

--- test/index.js
+++ test/index.js
@@ -2,22 +2,53 @@ var expect = require("chai").expect;
 var request = require("request");
 
 describe('/contacts', function(){
-    before(function(){
-
-    });
-    it("has 200 status", function(done){
+    var res;
+    var parsedResponse;
+    beforeEach(function(done){
       request("http://localhost:3000/contacts", function(error, response, body){
-        expect(response.statusCode).to.equal(200);
+        res = response;
+        parsedResponse = JSON.parse(res.body);
         done();
       });
+    });
+
+    it("has 200 status", function(done){
+      expect(res.statusCode).to.equal(200);
+      done();
     });
 
     it("returns an array of contacts", function(done){
-      request("http://localhost:3000/contacts", function(error,response,body){
-        // expect(response.body).to.contain("objects")
-        var parsedResponse = JSON.parse(response.body);
-        expect(parsedResponse.response.objects).to.have.length.above(0);
-        done();
-      });
+      expect(parsedResponse.response.objects).to.have.length.above(0);
+      done();
     });
+
+        describe('Contact', function(done){
+          var res;
+          var parsedResponse;
+          var contact;
+
+        before(function(done){
+          request("http://localhost:3000/contacts", function(error, response, body){
+            res = response;
+            parsedResponse = JSON.parse(res.body);
+            contact = parsedResponse.response.objects[0];
+            done();
+          });
+        });
+        it("has all fields of contact", function(done){
+          var props =["name","email","isgc"]
+          expect(contact.properties).to.include.keys(props)
+          done();
+        });
+
+        it("contains i.d", function(done){
+          expect(contact.id).to.exist
+          expect(contact.id.length).to.be.above(23)
+          done();
+        });
+        it("contains email", function(done){
+          expect(contact.properties.email[0].value).to.contain('@');
+          done();
+        })
+      });
 });
